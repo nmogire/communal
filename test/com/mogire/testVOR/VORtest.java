437efabad6647638e43032e0b44c2fbc4a94e273
Nancy Mogire <nancymogire@gmail.com>
2014-04-19 20:42:40 -1000

    Added testcase

---
+++ test/com/mogire/testVOR/VORtest.java
@@ -0,0 +1,24 @@
+package com.mogire.testVOR;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class VORtest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
